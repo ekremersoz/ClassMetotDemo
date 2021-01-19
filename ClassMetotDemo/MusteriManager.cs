c773c5723fe66b5b0ad5af64bc6137972b5f231b
ekremersoz <ekremersoz06@gmail.com>
2021-01-20 01:05:15 +0300

    Ödev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi :"+musteri.Id + " "+musteri.Adi+" "+musteri.Soyadi+" "+musteri.Sehir);
+        }
+        public void Listeleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi :"+musteri.Id+" "+musteri.Adi+" "+musteri.Soyadi+" "+musteri.Sehir);
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi :"+musteri.Id+" "+musteri.Adi+""+musteri.Soyadi+" "+musteri.Sehir);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,39 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.Adi = "Ekrem";
+            musteri.Soyadi = "Ersöz";
+            musteri.Sehir = "Ankara";
+
+            Musteri musteri2 = new Musteri
+            {
+                Id = 2,
+                Adi ="Baran",
+                Soyadi = "Düzgün",
+                Sehir = "Sivas"
+            };
+
+            Musteri muster3 = new Musteri
+            {
+                Id = 3,
+                Adi = "Onur",
+                Soyadi = "Yılmaz",
+                Sehir = "Yozgat"
+            };
+            
+            Musteri[] musteriler = new Musteri [] {musteri , musteri2,muster3 };
+
+            foreach (Musteri item in musteriler)
+            {
+                musteriManager.Listeleme(item);
+            }
+
+            musteriManager.Ekleme(musteri);
+            musteriManager.Silme(musteri2);
         }
     }
 }
